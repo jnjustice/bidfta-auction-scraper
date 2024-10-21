80f060d32bfa392c9da81e5a02818d0bb0ed0071
jnjustice <119473853+jnjustice@users.noreply.github.com>
2024-10-20 20:36:50 -0400

    Update launch_auction_viewer.bat
    adjusted hierarchy for frontend/

--- scripts/launch_auction_viewer.bat
+++ scripts/launch_auction_viewer.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Python HTTP server on port 8000...
-start "" http://localhost:8000/auction_viewer.html
+start "" http://localhost:8000/frontend/auction_viewer.html
 python -m http.server 8000
 echo Server stopped.
 pause
